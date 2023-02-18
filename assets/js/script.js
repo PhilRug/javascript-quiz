2d7c24a233b0a986bfaaef2fa5d05d105aad4283
PhilRug <ruggiero.pj@gmail.com>
2023-02-17 23:26:32 -0500

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-var timerEl = document.getElementById('timer');
+var countDownDate;
+var countdownTimer;
+
+document.getElementById("countdown-btn").addEventListener("click", function() {
+  countDownDate = new Date().getTime() + 75000;
+  countdownTimer = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var seconds = Math.floor((distance % (1000 * 75)) / 1000);
+    document.getElementById("countdown").innerHTML = "Time remaining: " + seconds + "s";
+    if (distance < 0) {
+      clearInterval(countdownTimer);
+      document.getElementById("countdown").innerHTML = "Time's up!";
+    }
+  }, 1000);
+});
